2d50ca5897989b48f5872785cc8ac7045832c97e
Ernesto Sandoval <ersandoval48@gmail.com>
2023-10-10 21:19:26 -0700

    removed removalDelay from _main.js

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -104,7 +104,7 @@ $(function() {
     image: {
       tError: '<a href="%url%">Image #%curr%</a> could not be loaded.'
     },
-    removalDelay: 500, // Delay in milliseconds before popup is removed
+    removalDelay: 0, //500, // Delay in milliseconds before popup is removed
     // Class that is added to body when popup is open.
     // make it unique to apply your CSS animations just to this exact popup
     mainClass: "mfp-zoom-in",
